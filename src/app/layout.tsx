365925f985fb96ac05755ec0cfc06e53961e1654
PeppermintSnow <peppermintsnow06@gmail.com>
2025-04-28 19:32:14 -0800

    Changed bg attachment to fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        style={{backgroundColor: '#101828', height: '100vh'}}
+        style={{backgroundColor: '#101828', height: '100vh', backgroundAttachment: 'fixed'}}
       >
         <Analytics />
         <Navbar />
