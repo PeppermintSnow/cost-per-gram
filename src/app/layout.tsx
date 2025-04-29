c579fcbc3525414933af1a4e602f3baa2fb9a08f
PeppermintSnow <peppermintsnow06@gmail.com>
2025-04-28 19:04:29 -0800

    Changed bg height to take up the entire screen height

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        style={{backgroundColor: '#101828'}}
+        style={{backgroundColor: '#101828', height: '100vh'}}
       >
         <Analytics />
         <Navbar />
